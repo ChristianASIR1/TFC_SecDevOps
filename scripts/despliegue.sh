1a70eb565e065380935c68ce8c842002f2ab590f
ChristianASIR1 <christianpenagomez95@liceolapaz.net>
2026-04-29 17:00:44 +0200

    feat: Despliegue inicial automatizado 100% funcional

--- scripts/despliegue.sh
+++ scripts/despliegue.sh
@@ -27,28 +27,36 @@ done
 
 
 # Ansible #
-echo "Ejecutando Playbooks de Ansible..."
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_SSH_COMMON_ARGS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+LLAVE_SSH="/home/walter/v2_secdevops/ssh_keys/ssh_tfc_v2"
+IP_REDTEAM=$(grep -A 1 "\[redteam\]" ansible/hosts.ini | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1)
+
 
 echo "Ejecutando 1/6: Despliegue inicial..."
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/despliegue_inicial.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/despliegue_inicial.yml --private-key "$LLAVE_SSH"
 
 echo "Ejecutando 2/6: Hardening SSH"
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/hardening_ssh.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/hardening_ssh.yml --private-key "$LLAVE_SSH"
 
 echo "Ejecutando 3/6: Instalación Wazuh Manager"
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/wazuh_server.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/wazuh_server.yml --private-key "$LLAVE_SSH"
 
 echo "Ejecutando 4/6: Despliegue Podman, DVWA y Wazuh Agent"
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/podman_wazuh.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/podman_wazuh.yml --private-key "$LLAVE_SSH"
 
 echo "Ejecutando 5/6: Despliegue WAF"
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/instalar_waf.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/instalar_waf.yml --private-key "$LLAVE_SSH"
 
 echo "Ejecutando 6/6: Preparación herramientas Red Team"
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/preparar_redteam.yml
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/preparar_redteam.yml --private-key "$LLAVE_SSH"
+
+
 
 
 echo "[EXITO]  Infraestructura SecDevOps desplegada"
 echo " Manager Wazuh: https://10.0.0.10"
 echo " Aplicación DVWA: http://10.0.0.20:8080"
-echo " Contraseñas guardadas como: credenciales_wazuh_v2.txt"
+echo "MV Red-Team:  ${IP_REDTEAM:-IP_No_Encontrada}"
+echo " Contraseñas guardadas en: /home/walter/v2_secdevops/credenciales_wazuh.txt"
