d21c2e393ff5599d8f9bb6ddbd73d8626dc1cd11
ChristianASIR1 <christianpenagomez95@liceolapaz.net>
2026-04-30 20:40:44 +0200

    [FIX] Corrección borrado de hosts conocidos variable

--- scripts/despliegue.sh
+++ scripts/despliegue.sh
@@ -7,7 +7,7 @@
 # Variables #
 DIR_PROYECTO="/home/walter/v2_secdevops"
 LLAVE_SSH="$DIR_PROYECTO/ssh_keys/ssh_tfc_v2"
-IP_REDTEAM=$(grep -A 1 "\[redteam\]" ansible/hosts.ini 2>/dev/null | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1)
+IP_REDTEAM=$(cat /home/walter/v2_secdevops/ansible/hosts.ini | grep redteam | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 
 
 # Parar la ejecución si falla algun comando:
@@ -19,8 +19,9 @@ set -e
 echo "Limpiando claves SSH antiguas de ejecuciones previas..."
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "10.0.0.10" >/dev/null 2>&1 || true
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "10.0.0.20" >/dev/null 2>&1 || true
+ 
 if [ -n "$IP_REDTEAM" ]; then
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$IP_REDTEAM" >/dev/null 2>&1 || true
+    ssh-keygen -f '/home/walter/.ssh/known_hosts' -R "$IP_REDTEAM" >/dev/null 2>&1 || true
 fi
 
 
